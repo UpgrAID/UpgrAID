e5e0ab7efcc0398ef13fab358432a1acaff4293a
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2015-12-13 15:08:04 -0800

    serializer fixs

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
-
-# Create your tests here.
+# from django.contrib.auth.models import User
+# from django.core.urlresolvers import reverse
+# from rest_framework.test import APITestCase
+# from user.models import Theme
+#
+#
+# class PostTests(APITestCase):
+#
+#     def setUp(self):
+#         self.user = User.object.create_user('test', email='test@test.com',
+#                                             password='testpassword')
+#         self.theme = Theme.objects.create(title='TestTheme')
+#         self.goal = Goal.objects.create()
+#         self.post = Post.objects.create()
+#
+#     def test_post_list
