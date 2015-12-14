c0124ab7ddcc5b1961a91e7ca329ef5dbff382dd
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2015-12-14 09:54:49 -0800

    fixing migrations

--- user/migrations/0007_auto_20151214_1743.py
+++ user/migrations/0007_auto_20151214_1743.py
@@ -21,6 +21,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='profile',
             name='rank',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='user.Rank'),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='user.Rank'),
         ),
     ]
