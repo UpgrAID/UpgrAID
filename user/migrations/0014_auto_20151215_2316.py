56035ba773dbadfa7e40cc1e214958543edf6af6
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2015-12-15 18:53:22 -0800

    rank and achievements done

--- user/migrations/0014_auto_20151215_2316.py
+++ user/migrations/0014_auto_20151215_2316.py
@@ -16,6 +16,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='profile',
             name='rank',
-            field=models.ForeignKey(default=12, on_delete=django.db.models.deletion.CASCADE, to='user.Rank'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='user.Rank'),
         ),
     ]
