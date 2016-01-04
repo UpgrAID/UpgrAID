0ef30b3dc32f5b0a9c83a1eedd97f3b106df7e53
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2016-01-04 15:16:30 -0800

    sender permissions

--- post/permissions.py
+++ post/permissions.py
@@ -9,3 +9,13 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.user == request.user
+
+
+class IsSenderOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.sender == request.user
