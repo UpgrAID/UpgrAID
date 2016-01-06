a4f757e5f9c674fb353eb395e75d10c32b1932e0
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2016-01-05 18:24:55 -0800

    goal tests

--- post/tests.py
+++ post/tests.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from post.models import Goal, Post, Comment
+from rest_framework import status
 from rest_framework.test import APITestCase
 from user.models import Theme
 
@@ -20,10 +21,29 @@ class PostTests(APITestCase):
                                               description='test comment',
                                               user=1)
 
-    # def test_goal_list(self):
-    #     url = reverse('api_goal_list_create')
-    #     response = self.client.get(url, {}, format='json')
-    #
+    def test_goal_list(self):
+        url = reverse('api_goal_list_create')
+        response = self.client.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 1)
+
+    def test_goal_create(self):
+        url = reverse('api_goal_list_create')
+        response = self.client.post(url, {"title": "test goal", "theme": 1},
+                                   format='json')
+        response2 = self.client.post(url, {"title": "I want to dance",
+                                           "theme": 1}, format='json')
+        self.client.force_authenticate(user=self.user)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Goal.objects.count(), 3)
+        self.assertEqual(self.user.id, response.data['user'])
+        self.assertNotEqual(response.data['group'], response2.data['group'])
+        self.assertEqual(response.data['group'], self.goal.group)
+        goal = Goal.objects.get(pk=response.data['id'])
+        self.assertEqual(len(goal.similar_goal_list()), 2)
+        self.assertEqual((goal.closest_goal(goal.similar_goal_list())), self.goal)
+
+
     # def test_post_list(self):
     #     url = reverse('api_post_list_create')
     #     response = self.client.get(url, {}, format='json')
