7822e5a4e8d124902dc5dfb5ce9f8b6270eea704
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2016-01-06 01:55:49 -0800

    fixing test

--- post/tests.py
+++ post/tests.py
@@ -55,12 +55,14 @@ class PostTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post_create(self):
-        post_achievement = Achievement.objects.create(name='test achievement', type='Post', required_amount=1)
+        post_achievement = Achievement.objects.create(name='test achievement',
+                                                      type='Post',
+                                                      required_amount=1)
         url = reverse('api_post_list')
         self.client.force_authenticate(user=self.user)
         response = self.client.get(url, {"title": "test post 2",
                                          "description": "test description 2",
-                                         "group": self.goal.group},
+                                         "group": self.goal.group.pk},
                                    format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), 2)
