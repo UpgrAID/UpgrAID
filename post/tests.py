9da9c679be5b0569f78be04d4334598e661d38b3
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2016-01-07 20:03:34 -0800

    middleware test

--- post/tests.py
+++ post/tests.py
@@ -63,10 +63,7 @@ class PostTests(APITestCase):
         self.assertEqual(len(goal.similar_goal_list()), 5)
         self.assertEqual((len(goal.closest_goal(goal.similar_goal_list()))), 2)
         self.assertEqual(goal.closest_goal(goal.similar_goal_list())[1][0], self.goal)
-        self.user.profile.last_active = datetime.date.today() - datetime.timedelta(days=50)
-        self.user.profile.activity()
-        self.user.profile.save()
-        self.assertEqual(goal2.inactive, True)
+        self.assertEqual(self.user.profile.last_active, datetime.date.today())
 
     def test_post_list(self):
         url = reverse('api_post_list')
